765630f43502171697994774b342fd5a1f5be5fb
BALAJINR2023 <ravibalajinr@gmail.com>
2024-04-26 21:39:42 +0530

    update

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -3,11 +3,11 @@ import PropTypes from "prop-types";
 const Cards = ({id, imageurl, des, author, date, tag }) => {
   return (
     <div className="card" style={{width: '18rem', display: 'flex', flexDirection: 'column', justifyContent: 'space-between',marginBottom: '10px'}}>
-    <img className="card-img-top" src={imageurl} alt="Card image cap"/>
+    <img className="card-img-top" src={imageurl} alt="Card image cap" style={{ width: '18rem', height: '200px' }}/>
     <div className="card-body">
-    <h4 className="card-title">{des}</h4>
-    <p className="card-text">{author}</p>
-    <p className="card-text">{date}</p>
+    <h4 className="card-title">{des}</h4><br></br>
+    <p className="card-text"><b>AUTHOR NAME:</b> {author}</p>
+    <p className="card-text"><b>DATE:</b> {date}</p>
   </div>
 </div>
   )
