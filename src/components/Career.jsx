d7537d0f8d85c0c33b58b6c146d32b9df732d04f
BALAJINR2023 <ravibalajinr@gmail.com>
2024-04-26 22:00:09 +0530

    IMG UPD

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import IMG from "../assets/CR1.webp"
 
 const Career = () => {
   return (
     <>
     <div>
     <h2 style={{color: 'blue', textAlign: 'center' }}>CAREER</h2>
-    <img src="https://www.guvi.in/img/flow.png" class=" img-fluid" alt=""/>
+    <img src={IMG}  class=" img-fluid" alt=""/>
     <p style={{color: 'blue', textAlign: 'center', textTransform: 'uppercase', marginTop: '20px'  }}>Plan your career and achieve success!</p>
     </div>
     </>
